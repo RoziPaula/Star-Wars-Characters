e32aff23d89bdc20ffe4594592fcd43cbeedf734
RoziPaula <rozi.paula@hotmail.com>
2024-05-15 18:26:11 -0300

    final project

---
+++ script.js
@@ -0,0 +1,173 @@
+let currentPageUrl = 'https://swapi.dev/api/people/';
+
+window.onload = async () => {
+    try {
+       await loadCharacters(currentPageUrl);
+    } catch (error) {
+        console.log(error);
+        alert('Erro ao carregar cards');
+    }
+
+    const nextButton = document.getElementById('next-button')
+    const backButton = document.getElementById('back-button')
+
+    nextButton.addEventListener('click', loadNextPage)
+    backButton.addEventListener('click', loadPreviousPage)
+
+};
+
+async function loadCharacters (url) {
+    const mainContent = document.getElementById ('main-content')
+    mainContent.innerHTML = ''; //Limpa os elementos da página anterior antes de gerar os elementos da próxima página.
+
+    try {
+
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        responseJson.results.forEach((character) => {
+            const card = document.createElement("div")
+            card.style.backgroundImage = 
+            `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g, "")}.jpg')` 
+            card.className = "cards"
+
+            const characterNameBG = document.createElement("div")
+            characterNameBG.className = "character-name-bg"
+
+            const characterName = document.createElement("span")
+            characterName.className = "character-name"
+            characterName.innerText = `${character.name}`
+
+            characterNameBG.appendChild(characterName)
+            card.appendChild(characterNameBG)
+
+            card.onclick = () => {
+                const modal = document.getElementById("modal")
+                modal.style.visibility = "visible"
+
+                const modalContent = document.getElementById("modal-content")
+                modalContent.innerHTML = ''
+
+                const characterImage = document.createElement("div")
+                characterImage.style.backgroundImage = 
+                `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g, "")}.jpg')`
+                characterImage.className = "character-image"
+
+                const name = document.createElement("span")
+                name.className = "character-details"
+                name.innerText = `Nome: ${character.name}`
+
+                const characterHeight = document.createElement("span")
+                characterHeight.className = "character-details"
+                characterHeight.innerText = `Altura: ${convertHeight(character.height)}`
+
+                const mass = document.createElement("span")
+                mass.className = "character-details"
+                mass.innerText = `Peso: ${convertMass(character.mass)}`
+
+                const birthYear = document.createElement("span")
+                birthYear.className = "character-details"
+                birthYear.innerText = `Nascimento: ${convertBirthYear(character.birth_year)}`
+
+                const gender = document.createElement("span")
+                gender.className = "character-details"
+                gender.innerText = `Gênero: ${convertGender(character.gender)}`
+
+                modalContent.appendChild(characterImage)
+                modalContent.appendChild(name)
+                modalContent.appendChild(characterHeight)
+                modalContent.appendChild(mass)
+                modalContent.appendChild(birthYear)
+                modalContent.appendChild(gender)
+
+
+            }
+
+            mainContent.appendChild(card)
+        });
+
+
+        const nextButton = document.getElementById('next-button')
+        const backButton = document.getElementById('back-button')
+
+        nextButton.disabled = !responseJson.next
+        backButton.disabled = !responseJson.previous
+
+        backButton.style.visibility = responseJson.previous? "visible" : "hidden"
+
+        currentPageUrl = url
+
+    } catch (error) {
+        alert('Erro ao carregar os personagens')
+        console.log(error)
+    }
+}
+
+async function loadNextPage() {
+    if(!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.next)
+
+    } catch (error) {
+        console.log(error)
+        alert ('Erro ao carregar a próxima página')
+    }
+}
+
+async function loadPreviousPage() {
+    if(!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.previous)
+
+    } catch (error) {
+        console.log(error)
+        alert ('Erro ao carregar a página anterior')
+    }
+}
+
+function hideModal() {
+    const modal = document.getElementById('modal')
+    modal.style.visibility = "hidden"
+}
+
+function convertGender(gender) {
+    const genero = {
+        male : "masculino",
+        female : "feminino",
+        "n/a" : "não se aplica"
+    };
+
+    return genero [gender.toLowerCase()] || gender;
+}
+
+function convertHeight(height) {
+    if (height === "unknown") {
+        return "desconhecida"
+    }
+
+    return (height / 100).toFixed(2);
+}
+
+function convertMass(mass) {
+    if (mass === "unknown") {
+        return "desconhecido"
+    }
+
+    return `${mass} kg`
+}
+
+function convertBirthYear(birthYear) {
+    if (birthYear === "unknown") {
+        return "desconhecido"
+    }
+
+    return birthYear
+}
